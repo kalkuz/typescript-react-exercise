de36e8a9305fdb146b76667819ed93151c64a83c
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2022-10-31 15:49:05 +0300

    9.15

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: 'normal';
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: 'submission';
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+function Header({ name }: { name: string }) {
+  return (
+    <h1>{name}</h1>
+  );
+}
+
+function Content({ part }: { part: CoursePartBase }) {
+  return (
+    <div>
+      <p>
+        <b>
+          {part.name}
+          {' '}
+          {part.exerciseCount}
+        </b>
+      </p>
+
+      {(part.type === 'normal' || part.type === 'submission') && (
+        <p>
+          <i>{(part as CourseNormalPart).description}</i>
+        </p>
+      )}
+      {part.type === 'groupProject' && (
+        <p>
+          project exercises
+          {' '}
+          {(part as CourseProjectPart).groupProjectCount}
+        </p>
+      )}
+      {part.type === 'submission' && (
+        <p>
+          submit to
+          {' '}
+          {(part as CourseSubmissionPart).exerciseSubmissionLink}
+        </p>
+      )}
+    </div>
+  );
+}
+
+function Total({ courseParts }: { courseParts: Array<any> }) {
+  return (
+    <p>
+      Number of exercises
+      {' '}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  );
+}
+
+// this is the new coursePart variable
+const courseParts: CoursePart[] = [
+  {
+    name: 'Fundamentals',
+    exerciseCount: 10,
+    description: 'This is the easy course part',
+    type: 'normal',
+  },
+  {
+    name: 'Advanced',
+    exerciseCount: 7,
+    description: 'This is the hard course part',
+    type: 'normal',
+  },
+  {
+    name: 'Using props to pass data',
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: 'groupProject',
+  },
+  {
+    name: 'Deeper type usage',
+    exerciseCount: 14,
+    description: 'Confusing description',
+    exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+    type: 'submission',
+  },
+];
 
 function App() {
+  const courseName = 'Half Stack application development';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={courseName} />
+      {courseParts.map((cp) => <Content key={cp.name} part={cp} />)}
+      <Total courseParts={courseParts} />
     </div>
   );
 }
